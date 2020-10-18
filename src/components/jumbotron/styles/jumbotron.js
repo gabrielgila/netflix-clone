307b8bb7b5524d169af73db48f2800a3079d9645
mgila <marianogila@gmail.com>
2020-10-18 10:12:24 -0700

    styling

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -5,7 +5,7 @@ export const Inner = styled.div`
     align-items: center;
     flex-direction: ${({ direction }) => direction};
     justify-content: space-between;
-    max-width: 110px;
+    max-width: 1100px;
     margin: auto;
     
     @media (max-width: 1000px) {
@@ -17,10 +17,29 @@ export const Pane = styled.div`
     width: 50%;
     `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    font-size: 50px;
+    line-height: 1.1;
+    margin-bottom: 8px;
+    
+    @medioa (max-width: 600px) {
+        font-size: 35px;
+    }
+    `;
 
-export const SubTitle = styled.h2``;
+export const SubTitle = styled.h2`
+    font-size: 26px;
+    font-weight: normal;
+    line-height: normal;
+    
+    @medioa (max-width: 600px) {
+        font-size: 18px;
+    }
+    `;
 
-export const Image = styled.image``;
+export const Image = styled.img`
+    max-width: 100%;
+    height: auto;
+    `;
 
 export const Container = styled.div``;
