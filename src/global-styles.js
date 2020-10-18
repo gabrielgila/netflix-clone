6cab11685e2a55a567fa0a08f0a4ff402e51de03
mgila <marianogila@gmail.com>
2020-10-18 10:35:39 -0700

    global style

---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-familiy: 'Helvetica Neue', Helvetica, Ariel, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osex-font-smoothing: grayscale;
+        background-color: black;
+        color: #333333;
+        font-size: 16px;
+    }
+
+    `;
