2b9f43d8e25afed123a7fc0da77ee701d2f51254
mgila <marianogila@gmail.com>
2020-10-20 16:20:57 -0700

    footer ok

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -19,7 +19,7 @@ export const Column = styled.div`
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
   grid-gap: 15px;
   @media (max-width: 1000px) {
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import { Footer } from '../components';
 
 export function FooterContainer() {
-    return(
+    return (
         <Footer>
-            <Footer.Title>Questions? Contact Us</Footer.Title>
+            <Footer.Title>Questions? Contact us.</Footer.Title>
             <Footer.Break />
             <Footer.Row>
                 <Footer.Column>
@@ -16,7 +16,7 @@ export function FooterContainer() {
                 </Footer.Column>
 
                 <Footer.Column>
-                    <Footer.Link href="#">Help Center</Footer.Link>
+                    <Footer.Link href="#">Help Centre</Footer.Link>
                     <Footer.Link href="#">Jobs</Footer.Link>
                     <Footer.Link href="#">Terms of Use</Footer.Link>
                     <Footer.Link href="#">Contact Us</Footer.Link>
@@ -24,11 +24,20 @@ export function FooterContainer() {
 
                 <Footer.Column>
                     <Footer.Link href="#">Account</Footer.Link>
-                    <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+                    <Footer.Link href="#">Redeem gift cards</Footer.Link>
+                    <Footer.Link href="#">Privacy</Footer.Link>
+                    <Footer.Link href="#">Speed Test</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">Media Centre</Footer.Link>
+                    <Footer.Link href="#">Buy gift cards</Footer.Link>
                     <Footer.Link href="#">Cookie Preferences</Footer.Link>
                     <Footer.Link href="#">Legal Notices</Footer.Link>
                 </Footer.Column>
             </Footer.Row>
+            <Footer.Break />
+            <Footer.Text>Netflix United Kingdom</Footer.Text>
         </Footer>
-    )
+    );
 }
